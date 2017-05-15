16ce8064c6c014d9609168081bab600dc7898c44
Jonathan <cjs840312@gmail.com>
2017-05-15 15:36:16 +0800

    temp

--- src/App.js
+++ src/App.js
@@ -5,15 +5,40 @@ class App extends Component {
 
 	constructor(props) {
 	    super(props);
-        this.state = {};
+        this.state = {Requisite:{},Advice:{Name:'',img:'',link:''},Mode:'Advice'};
+	}
+
+	pushRequisite(){
+		console.log(this.state.Requisite[0])	
+		console.log(this.state.Requisite[1])
+		this.setState({Advice:{
+			Name:'HandMake Rose',
+			img:'http://www.zhidiy.com/uploadfile/201304/17/04155586.jpg',
+			link:'http://www.zhidiy.com/zhimeigui/5529.html'
+		}})
+	}
+	getAdvice(){
+		this.pushRequisite()
+	}
+
+	returnFeature(requisite){
+		this.setState({Requisite:requisite})
 	}
 
 	render() {
 		return(
 			<div className={styles.App}>
                 <h1> Gift Advisor </h1>
-				<FeatureForm/>
-				<button className={styles.PickGiftButton}> Pick a Gift </button>
+				<FeatureForm returnFeature={this.returnFeature.bind(this)}/>
+				<div style={{height:"60px"}}>
+					<button className={styles.PickGiftButton} onClick={this.getAdvice.bind(this)}> Pick a Gift </button>
+				</div>
+				{
+					this.state.Advice.img !== '' ? 
+					<Advice name={this.state.Advice.Name} img={this.state.Advice.img} link={this.state.Advice.link}/>
+					: null
+					}
+				}
 			</div>
 		);
 	
@@ -24,32 +49,58 @@ class FeatureForm extends Component {
 
 	constructor(props) {
 	    super(props);
-        this.state = {};
+        this.state = {feature:[
+			{ Name:"Holliday",Choice:["Mother's Day","Father's Day","Valentine's Day"],idx:0},
+			{ Name:'Type',Choice:["HandMake","Resturant","Shopping"],idx:0}
+		]};
+	}
+
+	returnFeature(newFeature){
+		let summary=[]
+		for(let n=0;n<newFeature.length;n++){
+			summary=summary.concat({Name:newFeature[n].Name, Choice:newFeature[n].Choice[newFeature[n].idx]})
+		}
+		this.props.returnFeature(summary)
+	}
+	
+	componentDidMount(){
+      this.returnFeature(this.state.feature)
+   	}
+
+	ChangeIdx(FeatureIdx, ChoiceIdx){
+		let newFeature=this.state.feature
+		newFeature[FeatureIdx].idx=ChoiceIdx
+		this.returnFeature(newFeature);
+		this.setState({feature:newFeature})
 	}
 
 	render(){ 
 		return(
 			<div className={styles.FeatureForm}>
-				<ChoiceBar/>
-				<ChoiceBar/>
+				{
+					this.state.feature.map((item,idx) => (
+						<ChoiceBar key={item.Name} Name={item.Name} Choice={item.Choice} ChangeIdx={this.ChangeIdx.bind(this,idx)}/>
+               		))
+				}
 			</div>
 		)
 	};
+
 };
 
 class ChoiceBar extends Component{
 	constructor(props) {
 	    super(props);
-        this.state = {};
+        this.state = {Name:this.props.Name,Choice:this.props.Choice};
 	}
 
 	render(){ 
 		return(
 			<div className={styles.ChoiceBar}>
 				<div className={styles.FeatureName}>
-					 <span style={{position:'relative',top:'3px'}}> Holliday</span> 
+					 <span style={{position:'relative',top:'3px'}}> {this.state.Name}</span> 
 				</div>
-				<FeatureList/>
+				<FeatureList Choice={this.state.Choice} ChangeIdx={this.props.ChangeIdx}/>
 			</div>
 		)
 	};
@@ -58,17 +109,49 @@ class ChoiceBar extends Component{
 class FeatureList extends Component{
 	constructor(props) {
 	    super(props);
-        this.state = {};
+        this.state = {choice:this.props.Choice,idx:0};
+	}
+
+	nextChoice(){
+		let newIdx=(this.state.idx + 1)%this.state.choice.length
+		this.setState({idx:newIdx})
+		this.props.ChangeIdx(newIdx)
+	}
+
+	prevChoice(){
+		let newIdx=(this.state.idx + this.state.choice.length - 1)%this.state.choice.length
+		this.setState({idx:newIdx})
+		this.props.ChangeIdx(newIdx)
 	}
 
 	render(){ 
 		return(
 				<div className={styles.FeatureList}>
-					<button style={{float:"left",marginLeft:"5%"}}>&#9668;</button>
-					<button style={{float:"right",marginRight:"5%"}}>&#9658;</button>
+					<button style={{float:"left"}} onClick={this.prevChoice.bind(this)}>&#9668;</button>
+						{this.state.choice[this.state.idx]}
+					<button style={{float:"right"}} onClick={this.nextChoice.bind(this)}>&#9658;</button>
 				</div>
 		)
 	};
 };
+
+class Advice extends Component{
+	constructor(props) {
+	    super(props);
+        this.state = {};
+	}
+
+	render(){ 
+		return(
+				<div className={styles.AdviceBlock}>
+					<form>
+						<div> {this.props.name} </div>
+						<img src={this.props.img}/>
+						<div> <a href={this.props.link}> link </a> </div>
+					</form>
+				</div>
+		)
+	};
+}
 
 export default App;
